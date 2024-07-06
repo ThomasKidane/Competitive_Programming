15eb83b6fe1697cc5cbe9a8bc5a5b1c641bbf289
ThomasGodThe2 <105876995+ThomasGodThe2@users.noreply.github.com>
2024-07-06 23:57:59 +0300

    Solved 1650D and 1003C

---
+++ Codeforce Problems/1003C.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+//#define int long long
+#define forn(i,n) for(int i=0;i<n;i++)
+
+signed main(){
+    int n,k;
+    cin>>n>>k;
+    cerr.precision(15);
+	cout.precision(15);
+	cerr << fixed;
+	cout << fixed;
+    long double Max=0;
+    int a[n];
+    int pf[n+1]={0};
+    for(int i=0;i<n;i++)cin>>a[i];
+    forn(i,n) pf[i+1]=pf[i]+a[i];
+    for(int x=0;x<=n-k;x++){
+        for(int y=x+k-1;y<n;y++){
+            Max=max(Max,(long double)(pf[y+1]-pf[x])/(y-x+1));
+        }
+    }
+    cout<<Max<<'\n';
+
+}
+
+
+
